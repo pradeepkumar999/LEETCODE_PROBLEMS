a5715c1865f0d52fc8a4e3538c06f1c9a1a90257
PRADEEP KUMAR YERRAJAMAN <91797100+pradeepkumar999@users.noreply.github.com>
2023-10-18 21:46:42 +0530

    Time: 0 ms (100%), Space: 40.2 MB (74.55%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder str=new StringBuilder();
+        for(char c :address.toCharArray()){
+            str.append((c=='.')?"[.]":c);
+        }
+        return str.toString();
+}
+}
